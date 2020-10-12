d3dc91cb2ceafc1eb02da973564477400f39a103
sapranidi <sapranidi@users.noreply.github.com>
2020-10-12 22:48:55 +0300

    Merged bsc-private scripts & Updated readme

--- test-bsc-private-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose up --build -d geth-bsc-build 
-docker-compose run golang sh -c 'cd /go/src/rh_tests/solidity/0.7; cp config.geth.json config.json; go run rh_tests && go test'
-
-
