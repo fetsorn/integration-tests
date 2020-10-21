828d7528004fa2010d215335c080f21ce2be9298
riotpurpp <shamfama@gmail.com>
2020-10-21 16:20:56 +0000

    Rework script messaging

--- symlink.sh
+++ symlink.sh
@@ -3,12 +3,26 @@
 # TRON part needs to be copied to core
 # and then referenced to.
 
+eth_contracts_dest='solidity/0.7/source'
+waves_contracts_dest='waves/scripts'
 
 # ETH
-ln -s "$(pwd)/core/contracts/ethereum/contracts" 'solidity/0.7/source'
+if [ -d "$eth_contracts_dest" ]
+then
+  echo "Symlink to $eth_contracts_dest already exists"
+else
+  ln -s "$(pwd)/core/contracts/ethereum/contracts" "$eth_contracts_dest"
+  echo "Created symlink from core to $eth_contracts_dest"
+fi
 
 # WAVES
-ln -s "$(pwd)/core/contracts/waves/script" 'waves/scripts'
+if [ -d "$waves_contracts_dest" ]
+then
+  echo "Symlink to $waves_contracts_dest already exists"
+else
+  ln -s "$(pwd)/core/contracts/waves/script" "$waves_contracts_dest"
+  echo "Created symlink from core to $waves_contracts_dest"
+fi
 
 # TRON
 # ln -s "$(pwd)/core/contracts/ethereum/contracts" 'solidity/tron/source'
