ca5ba99b14dcacd94f661dc2832c495bda072f4b
fetsorn <a.@s-MacBook-Air.local>
2021-02-15 13:48:05 +0300

    Add integration for HECO with custom geth

---
+++ test-heco-private.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#WORK IN PROGRESS
+
+docker-compose up --build -d geth-heco-build
+docker-compose run golang sh -c 'cd /go/src/rh_tests/solidity/0.7; cp config.geth-heco.json config.json; go get -u github.com/Gravity-Tech/gravity-core@master; go run rh_tests && go test'
